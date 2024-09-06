7269d296af9a1e58e72e132445aca638f33f1b1d
Rose <rose.megirian@gmail.com>
2024-09-06 11:50:37 +0800

    add welcome message and collect the filename

--- goostats.sh
+++ goostats.sh
@@ -1,3 +1,7 @@
 # Load a given file
 # Compute the min/max/range of values in a file
+fname=$1
+echo "working with file:"
+echo "welcome to Nell's stats script"
 
+# compute the min/max/range of values in a file.
